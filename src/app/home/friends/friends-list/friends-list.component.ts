af7ffc9a82ae90a4a396c187d11c6ddcb0f59560
domenica <rssdnc98t71h224g@studenti.unirc.it>
2023-07-14 17:57:24 +0200

    DA FINIRE POST E SISTEMARE IL METODO SEARCH FRIENDS

--- src/app/home/friends/friends-list/friends-list.component.ts
+++ src/app/home/friends/friends-list/friends-list.component.ts
@@ -8,60 +8,62 @@ import {FormControl, FormGroup} from "@angular/forms";
   templateUrl: './friends-list.component.html',
   styleUrls: ['./friends-list.component.css']
 })
-export class FriendsListComponent implements OnInit{
-  friends=new Array<User>()
-  loading=true;
-  errore:boolean=false;
+export class FriendsListComponent implements OnInit {
+  friends = new Array<User>()
+  loading = true;
+  errore: boolean = false;
+  searchInput = ""
 
-  friendsform: FormGroup = new FormGroup({
-    search: new FormControl(null),
-  })
+
   constructor(private requestService: RequestService) {
   }
 
 
-  ngOnInit(){
+  ngOnInit() {
     this.getFriends();
   }
-  getFriends(){
-    this.loading=true
-    return this.requestService.get("users").subscribe((res:any)=> {
-       console.log(res);
-      this.friends=res;
-    },(error) => {
+
+  update(value: string) {
+    this.searchInput = value;
+  }
+
+  getFriends() {
+    this.loading = true
+    return this.requestService.get("users").subscribe((res: any) => {
+      console.log(res);
+      this.friends = res;
+    }, (error) => {
       console.error(error);
-      this.loading=error;
-      this.errore=true;
+      this.loading = error;
+      this.errore = true;
 
       // 2 La chiamata è andata in errore e di conseguenza bisognerebbe notificare l'utente
     }, () => {
       // 3 La chiamata è stata completata con successo (SENZA ERRORI)
-      this.loading=false;
+      this.loading = false;
       this.salvaIdUserAttuale();
-      this.errore=false;
+      this.errore = false;
 
 
     })
   }
 
-  salvaIdUserAttuale(){
-    for(let i=0; i<this.friends.length;i++){
-      if(this.friends[i].username==localStorage.getItem("username")){
-        localStorage.setItem("id",String(this.friends[i].id));
-        this.friends.splice(i,1);
+  salvaIdUserAttuale() {
+    for (let i = 0; i < this.friends.length; i++) {
+      if (this.friends[i].username == localStorage.getItem("username")) {
+        localStorage.setItem("id", String(this.friends[i].id));
+        this.friends.splice(i, 1);
       }
     }
   }
 
-//   searchFriends(){
-//     const c= this.friends.filter(value => value.username>= this.friendsform.value.search);
-//      console.log(c)
-// //dafare
-//
-//   }
+  searchFriends() {
+    console.log("ciao")
+    console.log(this.searchInput)
+    this.friends=this.friends.filter(value => value.username === this.searchInput);
 
 
-
+  }
 
 
 }
